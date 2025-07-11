bb0cb13e5869e48f51638ed2492fc8bd1b434e56
V3xes <168699239+V3xes@users.noreply.github.com>
2025-07-10 23:20:54 -0600

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll(".sidebar a").forEach(link =>
-  link.addEventListener("click", () => {
-    document.querySelectorAll(".sidebar a").forEach(link => link.classList.remove("active"));
-    link.classList.add("active");
-  })
-);
